06a7880a4f60c48f27483ae996aea97d6c0abea6
adil-pi <bakinovadil@gmail.com>
2024-07-16 19:41:15 +0600

    Add files via upload

---
+++ adik/main.py
@@ -0,0 +1,41 @@
+# while True:
+#     x1 = input("Введите:")
+
+#     if x1 == "+":
+#         break
+
+# print("Вы вышли из цикла!")
+
+
+
+# import time
+
+# print("Я спать")
+
+# time.sleep(5)
+
+# print("Я проснулся!")
+
+
+
+# mylist = (2, 3, 5, 7, 4, 4, 3, 2, 9, 0, 2)
+
+# for i in mylist:
+#     if i == 2:
+#         print("Двойка найдена!")
+
+
+
+# mylist = [123, 321, 567, 89, 4, 76]
+
+# print(mylist[4])
+
+
+
+# numbers = (6, 4, 9, 8, 8, 6, 5, 2)
+
+# for i in numbers:
+#     if i  %2 == 0:
+#         print(i)
+
+ 
